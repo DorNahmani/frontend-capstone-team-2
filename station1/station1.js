717928234ccc7e612275d4a0e736552b3d678da2
Meitar Arama <meitar143@gmail.com>
2026-01-12 19:04:22 +0200

    refactor: added a new JS code to station 1

---
+++ station1/station1.js
@@ -0,0 +1,28 @@
+
+
+const startBtn = document.querySelector("startBtn");
+
+const nameInput = document.querySelector("#userName");
+
+
+
+
+startBtn.addEventListener("click", function() {
+
+    let name = nameInput.value;
+
+
+    if (name.trim() === "") {
+
+        alert("עליך להזין שם כדי להתחיל במשימה!")
+
+    } else {
+
+        localStorage.setItem("playerName", name);
+
+        alert("בהצלחה " + name + "המעבדה נפתחת... !")
+
+        window.location.href = "../station2/station2.html"
+    }
+
+});
